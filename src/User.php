56ca1a09669929054d75c0e270cb51f0144eb0ad
Wati5 <Antoine_Barchon@etu.u-bourgogne.fr>
2024-05-16 11:07:03 +0200

    Ajout des entités Bug et User (partie2)

--- src/User.php
+++ src/User.php
@@ -1,28 +1,44 @@
 <?php
 // src/User.php
+
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection; 
 #[ORM\Entity]
 #[ORM\Table(name: 'users')]
 class User
 {
-/** @var int */
-#[ORM\Id]
-#[ORM\GeneratedValue]
-#[ORM\Column(type: 'integer')]
-private int|null $id = null;
-/** @var string */
-#[ORM\Column(type: 'string')]
-private string $name;
-public function getId(): int|null
-{
-return $this->id;
-}
-public function getName(): string
-{
-return $this->name;
-}
-public function setName(string $name): void
-{
-$this->name = $name;
-}
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string')]
+    private string $name;
+
+    #[ORM\OneToMany(targetEntity: "Bug", mappedBy: "reporter")]
+    private ArrayCollection $reportedBugs;
+
+    #[ORM\OneToMany(targetEntity: "Bug", mappedBy: "engineer")]
+    private ArrayCollection $assignedBugs;
+
+    public function __construct()
+    {
+        $this->reportedBugs = new ArrayCollection();
+        $this->assignedBugs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
 }
